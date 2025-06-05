c592b644a15e584bd3c3bb0068b99271a3bcb360
Bosu <badebosubabu@gmail.com>
2025-06-05 16:15:17 -0400

    Deleted all files to start with a fresh master branch

--- test.js
+++
@@ -1,7 +0,0 @@
-const { createVisitor, getVisitorInfo } = require('./index');
-
-const visitor = createVisitor({ name: 'Bosu Bade', email: 'bosu@example.com' });
-console.log(visitor);
-
-const info = getVisitorInfo('12345');
-console.log(info);
